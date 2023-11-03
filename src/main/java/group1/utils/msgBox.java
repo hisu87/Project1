9f98e26219ee38e21327c610d41c0ef56cde42cb
hisu87 <numpadagain@gmail.com>
2023-11-03 15:40:00 +0700

    Update file utils
    đây là các file chức năng giúp quá trình phát triển file nhanh hơn

---
+++ src/main/java/group1/utils/msgBox.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package group1.utils;
+
+/**
+ *
+ * @author numpa
+ */
+
+/* Đây là file để tạo thông báo nhanh hơn */
+public class msgBox {
+
+}
---
+++ src/main/java/group1/utils/xImages.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package group1.utils;
+
+/**
+ *
+ * @author numpa
+ */
+
+/* Đây là file để xử lý hình ảnh để ảnh lên label dễ dàng hơn */
+public class xImages {
+
+}
