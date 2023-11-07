004c3c11ed6dd1bd4cd33fe88ec2f629868266d8
hisu87 <numpadagain@gmail.com>
2023-11-07 16:10:00 +0700

    Refactor package name for JMainFrame.java

--- src/main/java/group1/views/JMainFrame.java
+++ src/main/java/group1/views/JMainFrame.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
  */
-package group1.ui;
+package group1.views;
 
 import group1.utils.xImage;
 import java.io.File;
